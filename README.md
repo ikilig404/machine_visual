85dd6a73981a62b42796f9c96fe545eab36f26ef
wjer <1790992954@qq.com>
2019-10-28 17:32:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 机器视觉
+
+## 入门博客
+
+  * <https://blog.csdn.net/Programmer_ch/article/details/89057129>
---
+++ learCamera.py
@@ -0,0 +1,14 @@
+import cv2
+
+# 引入库
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    cv2.imshow("Video", frame)
+    # 读取内容
+    if cv2.waitKey(10) == ord("q"):
+        break
+
+# 随时准备按q退出
+cap.release()
+cv2.destroyAllWindows()
